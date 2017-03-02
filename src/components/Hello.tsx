61ee65117f4a5f5bf66462fc437e1557852bd4d5
Matthew dB <matthewdb85@gmail.com>
2017-03-02 20:53:20 +0000

    VSCode test 3

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -4,6 +4,6 @@ export interface HelloProps {compiler: string; framework: string;}
 
 export class Hello extends React.Component<HelloProps, undefined> {
     render() {
-        return <h1>Hello from {props.compiler} and {props.framework} !</h1>;
+        return <h1>Hello from {props.compiler} and {props.framework}!</h1>;
     }
 }
